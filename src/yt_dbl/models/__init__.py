5893660e5df255ee25629aa6336cf1435b9a6539
Dmitriy Brolnickij <brolnickij@gmail.com>
2026-02-06 21:31:21 +0700

    feat(models): export public API from models package

--- src/yt_dbl/models/__init__.py
+++ src/yt_dbl/models/__init__.py
@@ -1,1 +1,13 @@
 """ML model management utilities."""
+
+from yt_dbl.models.manager import ModelManager, model_manager
+from yt_dbl.models.registry import MODEL_REGISTRY, ModelInfo, check_model_downloaded, get_model_size
+
+__all__ = [
+    "MODEL_REGISTRY",
+    "ModelInfo",
+    "ModelManager",
+    "check_model_downloaded",
+    "get_model_size",
+    "model_manager",
+]
